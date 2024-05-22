a2087eea8406b3cf35879613ddb59d6806b0b935
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-22 19:30:46 +0530

    test api route created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
 dotenv.config();
 const app = express();
 
@@ -12,3 +13,6 @@ mongoose.connect(process.env.MONGOURL).then(()=>{
 app.listen(3000,(req,res)=>{
     console.log('Server running on 3000')
 })
+
+// routes
+app.use('/api/user',userRoutes)
