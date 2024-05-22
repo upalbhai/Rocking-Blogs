9349505563a035115d4f2c066d1c8af9cb548bbc
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-22 18:43:34 +0530

    Database connection successfull

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
+mongoose.connect(process.env.MONGOURL).then(()=>{
+    console.log("Database connection successfull")
+}).catch((err)=>{
+    console.log(err)
+})
 app.listen(3000,(req,res)=>{
     console.log('Server running on 3000')
 })
