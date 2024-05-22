6ce33805f7f8b7f234a030613b33036b4a8ad7b7
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-22 20:17:45 +0530

    middle ware added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,19 @@
 import User from "../model/user.model.js";
 import bcryptjs from 'bcryptjs'
-export const signup =async(req,res)=>{
+import { errorHandler } from "../utils/errorHandler.js";
+export const signup =async(req,res,next)=>{
     const {username,email,password} = req.body;
     
-    if(!username || !email || !password || password==='' || username==='' || email==='' ){
-        return res.status(400).json({message:"Please fill all the fields" });
-    }
+    if (
+        !username ||
+        !email ||
+        !password ||
+        username === '' ||
+        email === '' ||
+        password === ''
+      ) {
+        next(errorHandler(400, 'All fields are required'));
+      }
     const hashedPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({
         username,
@@ -17,6 +25,6 @@ export const signup =async(req,res)=>{
         await newUser.save();
         res.json('signup successfull')
     } catch (error) {
-        res.status(500).json({message:error.message})
+        next(error)
     }
 }
--- api/model/user.model.js
+++ api/model/user.model.js
@@ -1,19 +1,19 @@
 import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    }
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
 },{timestamps:true})
 
 const User = mongoose.model('User',userSchema);
