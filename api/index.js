5de95875ecfc05c465167d9a5dda42b40c546618
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-22 19:58:08 +0530

    signup route api created

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 const app = express();
-
+app.use(express.json())
 mongoose.connect(process.env.MONGOURL).then(()=>{
     console.log("Database connection successfull")
 }).catch((err)=>{
@@ -16,3 +17,4 @@ app.listen(3000,(req,res)=>{
 
 // routes
 app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
