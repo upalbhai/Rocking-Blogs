65f1a42b55c5c8bde277b009dfd5fe72c327edd4
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-24 13:44:13 +0530

    update route created

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,62 @@
+import User from "../model/user.model.js"
+import { errorHandler } from "../utils/errorHandler.js"
+import bcrypt from 'bcryptjs'
 export const test = (req,res)=>{
     res.json({message:"Api isn working"})
 }
+
+export const updateuser = async (req, res, next) => {
+    try {
+      if (req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to update this user'));
+      }
+  
+      const updates = {};
+  
+      // Check and validate password
+      if (req.body.password) {
+        if (req.body.password.length < 6) {
+          return next(errorHandler(400, 'Password must be a minimum length of 6 characters'));
+        }
+        updates.password = await bcrypt.hash(req.body.password, 10);
+      }
+  
+      // Check and validate username
+      if (req.body.username) {
+        const username = req.body.username;
+        if (username.length < 7 || username.length > 20) {
+          return next(errorHandler(400, 'Username must be between 7 and 20 characters'));
+        }
+        if (username.includes(' ')) {
+          return next(errorHandler(400, 'Username cannot contain spaces'));
+        }
+        if (username !== username.toLowerCase()) {
+          return next(errorHandler(400, 'Username must be in lowercase'));
+        }
+        if (!/^[a-zA-Z0-9]+$/.test(username)) {
+          return next(errorHandler(400, 'Username must contain only letters and numbers'));
+        }
+        updates.username = username;
+      }
+  
+      // Check and add other updatable fields
+      if (req.body.email) updates.email = req.body.email;
+      if (req.body.profilePicture) updates.profilePicture = req.body.profilePicture;
+  
+      // Update the user
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        { $set: updates },
+        { new: true }
+      );
+  
+      if (!updatedUser) {
+        return next(errorHandler(404, 'User not found'));
+      }
+  
+      const { password, ...rest } = updatedUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateuser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.get('/test',test);
-
+router.put('/update/:userId',verifyToken,updateuser)
 export default router;
