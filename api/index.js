e1b965b5123d88f8a74d73f4114673369ab0949e
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-25 00:31:45 +0530

    index updated

--- api/index.js
+++ api/index.js
@@ -4,15 +4,12 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
-import path from 'path'
-dotenv.config();
-const app = express();
-app.use(express.static(path.join(__dirname,'/client/dist')))
+import path from 'path';
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client','dist','index.html'));
-})
+dotenv.config();
 const __dirname = path.resolve();
+
+const app = express();
 app.use(express.json());
 app.use(cookieParser())
 mongoose.connect(process.env.MONGOURL).then(()=>{
@@ -30,6 +27,16 @@ app.get('/',(req,res)=>{
 // routes
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
+
+
+
+//for render
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 
 //middleware
 app.use((err,req,res,next)=>{
