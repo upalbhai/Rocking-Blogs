e52d9127df4c47d38f31fd3805c3f9789e2b0478
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-25 00:52:11 +0530

    final

--- api/index.js
+++ api/index.js
@@ -20,9 +20,7 @@ mongoose.connect(process.env.MONGOURL).then(()=>{
 app.listen(3000,(req,res)=>{
     console.log('Server running on 3000')
 })
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
+
 
 // routes
 app.use('/api/user',userRoutes)
