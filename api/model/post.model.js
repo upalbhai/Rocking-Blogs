551978b3cced267dfdaa192eda62cdda6184b97e
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-25 15:18:50 +0530

    add create post api route

---
+++ api/model/post.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+    userId:{
+        type:String,
+        required:true,
+    },
+    content:{
+        type:String,
+        required:true,
+    },
+    title:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    image:{
+        type:String,
+        default:'https://images.app.goo.gl/qXNeL39HFsaHAvgMA'
+    },
+    category:{
+        type:String,
+        default:'uncategorized'
+    },
+},{timestamps:true});
+
+const Post = mongoose.model('Post',postSchema);
+export default Post;
