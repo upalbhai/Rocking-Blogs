f34bcdd8145cd6f62cf89a350719997fef8a9815
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-28 20:10:30 +0530

    show user post

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -3,7 +3,7 @@ import { verifyToken } from '../utils/verifyUser.js';
 import { createPost, getPosts } from '../controllers/post.controller.js';
 const router = express.Router();
 router.post('/createpost',verifyToken,createPost)
-router.get('/getpost',getPosts)
+router.get('/getposts',getPosts)
 
 
 export default router;
--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
+import { Table } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 export default function DashPost() {
+    const { currentUser } = useSelector((state) => state.user);
+    const [userPosts, setUserPosts] = useState([]);
+  
+    useEffect(() => {
+      const fetchPosts = async () => {
+        try {
+          const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+          const data = await res.json();
+            console.log(userPosts)
+          if (!res.ok) {
+            return toast.error(data.message || 'Failed to fetch posts');
+          }
+  
+          setUserPosts(data.posts);
+        } catch (error) {
+          toast.error(error.message || 'An error occurred');
+        }
+      };
+  
+      if (currentUser && currentUser.isAdmin) {
+        fetchPosts();
+      }
+    }, [currentUser._id]);
   return (
-    <div>
-      e5thehrg
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scroll-bar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scroll-bar-thumb-slate-500' >
+        {currentUser.isAdmin && userPosts.length>0 ? (
+           <>
+           <Table hoverable className='shadow-md'>
+             <Table.Head>
+               <Table.HeadCell>Date Updated</Table.HeadCell>
+               <Table.HeadCell>Post Image</Table.HeadCell>
+               <Table.HeadCell>Post Title</Table.HeadCell>
+               <Table.HeadCell>Category</Table.HeadCell>
+               <Table.HeadCell>Delete</Table.HeadCell>
+               <Table.HeadCell><span>Edit</span></Table.HeadCell>
+             </Table.Head>
+             <Table.Body className='divide-y'>
+               {userPosts.map((post) => (
+                 <Table.Row key={post._id} className='bg-white dark:border-gray-700 dark:bg-gray-800' >
+                   <Table.Cell>
+                     {new Date(post.updatedAt).toLocaleDateString()}
+                   </Table.Cell>
+                   <Table.Cell>
+                     <Link to={`/post/${post.slug}`}>
+                       <img className='h-10 w-20 object-cover bg-gray-500' src={post.image} alt={post.title} />
+                     </Link>
+                   </Table.Cell>
+                   <Table.Cell className='font-medium text-gray-900 dark:text-white' >{post.title}</Table.Cell>
+                   <Table.Cell>{post.category}</Table.Cell>
+                   <Table.Cell>
+                     {/* Implement the delete functionality */}
+                     <button><span className='font-medium text-red-500 hover:underline cursor-pointer' >Delete</span></button>
+                   </Table.Cell>
+                   <Table.Cell>
+                     <Link to={`/update-post/${post._id}`}>
+                       <button><span className='text-teal-500 hover:underline' >Edit</span></button>
+                     </Link>
+                   </Table.Cell>
+                 </Table.Row>
+               ))}
+             </Table.Body>
+           </Table>
+         </>
+        ):(
+            <p>no posts</p>
+        )}
     </div>
   )
 }
