54e327d5322338a89b2220ad3773ece681a6aec3
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-06-01 12:48:29 +0530

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,31 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import toast from 'react-hot-toast'
-import { useSelector } from 'react-redux'
-export default function Home() {
-  const {currentUser} = useSelector((state)=>state.user)
-  const [posts,setPosts] = useState({})
-  useEffect(()=>{
-    const fetchPosts =async ()=>{
-      const res= await fetch(`/api/post/getposts`);
-      const data = await res.json();
-      if(!res.ok){
-        return toast.error(data.message);
-      }
-      if(res.ok){
-        setPosts(data.posts)
-      }
-    }
-    fetchPosts();
-  },[])
-  return (
-    <div className='h-full p-5'>
-      <h1 className='text-slate-900 dark:text-slate-200 text-2xl' >Welcome to my Blog Website</h1>
-      <p className='textlg' >Here You will blogs related to the <span className='block'>sports like cricket, football, Kabaddi, etc</span></p>
-      <div className="">
-        {
+import { Link } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
+import { useEffect, useState } from 'react';
+import PostCard from '../components/PostCard';
 
-        }
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <div>
+      <div className='flex flex-col gap-6 p-28 px-6 max-w-6xl mx-auto '>
+      <h1 className='text-slate-900 dark:text-slate-200 text-3xl font-bold lg:text-6xl' >Welcome to my Blog Website</h1>
+      <p className='text-gray-500 text-xs sm:text-sm' >Here You will blogs related to the <span className='block'>sports like cricket, football, Kabaddi, etc</span></p>
+        <Link
+          to='/search'
+          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+        <CallToAction />
+      </div>
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='flex flex-wrap gap-4'>
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,43 +1,63 @@
 import { Button, Spinner } from 'flowbite-react';
-import React, { useEffect, useState } from 'react'
-import toast from 'react-hot-toast';
-import { Link, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
-export default function Post() {
-    const {postSlug} = useParams();
-    const [loading,setLoading] = useState(true);
-    const [post,setPost]=useState(null)
-    console.log(post)
-    useEffect(()=>{
-        const fetchPost = async()=>{
-            try {
-                setLoading(true);
-                const res = await fetch(`/api/post/getposts/?slug=${postSlug}`);
-                const data = await res.json();
-                if(!res.ok){
-                    setLoading(false)
-                    return toast.error(data.message)
-                }
-                if(res.ok){
-                    setPost(data.posts[0]);
-                    setLoading(false)
-                }
-            } catch (error) {
-                toast.error(error.message)
-                setLoading(false)
-            }
+import PostCard from '../components/PostCard';
+
+export default function PostPage() {
+  const { postSlug } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/post/getposts?slug=${postSlug}`);
+        const data = await res.json();
+        if (!res.ok) {
+          setError(true);
+          setLoading(false);
+          return;
         }
-        fetchPost();
-    },[postSlug])
-    if(loading){
-        return (
-            <div className='flex justify-center items-center min-h-screen' >
-                <Spinner size='xl' />
-            </div>
-        )
+        if (res.ok) {
+          setPost(data.posts[0]);
+          setLoading(false);
+          setError(false);
+        }
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchPost();
+  }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
     }
-  return  (
+  }, []);
+
+  if (loading)
+    return (
+      <div className='flex justify-center items-center min-h-screen'>
+        <Spinner size='xl' />
+      </div>
+    );
+  return (
     <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
       <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
         {post && post.title}
@@ -70,6 +90,13 @@ export default function Post() {
       </div>
       <CommentSection postId={post._id} />
 
+      <div className='flex flex-col justify-center items-center mb-5'>
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
     </main>
   );
 }
